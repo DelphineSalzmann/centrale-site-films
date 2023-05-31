dbaafd4deb5f1c9f0b7851387c003a4e67d07721
Salzmann Delphine <delphine.salzmann@student-cs.fr>
2023-05-31 16:44:46 +0200

    StarRating

--- frontend/src/components/Movie/Movie.jsx
+++ frontend/src/components/Movie/Movie.jsx
@@ -5,29 +5,11 @@ import { useState } from 'react';
 
 function Movie({ movie }) {
   // {"adult","backdrop_path","genre_ids""id","original_language","original_title","overview","popularity","poster_path","release_date","title","video","vote_average","vote_count"}
-
-  const [buttonPopup, setButtonPopup] = useState(false);
+  
+  const [buttonPopup, setButtonPopup]=useState(false);
   let img_path = '';
   img_path = 'https://image.tmdb.org/t/p/original';
   img_path = img_path.concat(movie.poster_path);
-
-  const App = () => {
-    const [isHover, setIsHover] = useState(false);
-
-    const handleMouseEnter = () => {
-      setIsHover(true);
-    };
-    const handleMouseLeave = () => {
-      setIsHover(false);
-    };
-
-    const movieStyle = () => {
-      '200px'; //width
-      'auto'; //height
-      '20px'; //border-radius
-      isHover ? 'lightblue' : 'rgb(0, 191, 255)'; //background color
-    };
-  };
 
   return (
     <div>
@@ -41,6 +23,6 @@ function Movie({ movie }) {
       <p className="Movie-title">{movie.original_title}</p>
     </div>
   );
-}
+};
 
 export default Movie;
