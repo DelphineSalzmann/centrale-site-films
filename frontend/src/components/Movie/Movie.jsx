a2c2cedd1c8d582862923f44853595bdac8125f1
Salzmann Delphine <delphine.salzmann@student-cs.fr>
2023-05-31 11:04:08 +0200

    popup

--- frontend/src/components/Movie/Movie.jsx
+++ frontend/src/components/Movie/Movie.jsx
@@ -1,17 +1,22 @@
+import Popup from '../Popup/Popup';
 import './Movie.css';
+import { useState } from 'react';
 
 function Movie({ movie }) {
   // {"adult","backdrop_path","genre_ids""id","original_language","original_title","overview","popularity","poster_path","release_date","title","video","vote_average","vote_count"}
+  
+  const [buttonPopup, setButtonPopup]=useState(false);
   let img_path = '';
   img_path = 'https://image.tmdb.org/t/p/original';
   img_path = img_path.concat(movie.poster_path);
 
   return (
     <div>
-      <img src={img_path} alt="logo" />
+      <button onClick={() => setButtonPopup(true)}><img src={img_path} alt="logo" /></button>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>Hello</Popup>
       <p className="Movie-title">{movie.original_title}</p>
     </div>
   );
-}
+};
 
 export default Movie;
