b869ef7104aba11c99c4dbda390bbfa16b27e8b5
Salzmann Delphine <delphine.salzmann@student-cs.fr>
2023-05-31 17:19:08 +0200

    essai

--- frontend/src/components/StarRating/StarRating.jsx
+++ frontend/src/components/StarRating/StarRating.jsx
@@ -1,34 +1,6 @@
-/*import React, { useState } from 'react';
-import Rating from 'react-simple-star-rating';
 
-const StarRating = ({ stars }) => {
-  const [rating, setRating] = useState(0); // initial rating value
+//npm install react-icons
 
-  // Catch Rating value
-  const handleRating = (rate) => {
-    setRating(rate);
-    // Some logic
-  };
-
-  return (
-    <div className="App">
-      <Rating
-        onClick={handleRating}
-        ratingValue={rating}
-        size={20}
-        label
-        transition
-        fillColor="orange"
-        emptyColor="gray"
-        className="foo" // Will remove the inline style if applied
-      />
-      {/* Use rating value */
-//      {rating}
-//   </div>
-// );
-//};
-
-//export default StarRating;
 import React, { useState } from 'react';
 import Rating from 'react-simple-star-rating';
 import {FaStar} from 'react-icons/fa';
