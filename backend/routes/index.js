6bc00a3fd52bba5d74e031cad5a6313d859ca577
Ekaterina Laurent <ekaterina.laurent@student-cs.fr>
2023-05-31 09:46:02 +0200

    me

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.json({ message: 'Hello from Express!' });
+  res.json({ message: 'Hello from me!' });
 });
 
 export default router;
