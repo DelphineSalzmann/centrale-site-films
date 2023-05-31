d7fe261e22d116f05594857170c4026ba8b7c294
Salzmann Delphine <delphine.salzmann@student-cs.fr>
2023-05-31 09:45:41 +0200

    slider

---
+++ frontend/src/components/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,83 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+const ImageSlider = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const timerRef = useRef(null);
+  const SliderStyle = {git 
+    height: '100%',
+    position: 'relative',
+  };
+  const leftArrowStyles = {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translate(0,-50%)',
+    left: '32px',
+    fontSize: '45px',
+    color: '#fff',
+    zIndex: 1,
+    cursor: 'pointer',
+  };
+  const rightArrowStyles = {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translate(0,-50%)',
+    right: '32px',
+    fontSize: '45px',
+    color: '#fff',
+    zIndex: 1,
+    cursor: 'pointer',
+  };
+  const SlideStyle = {
+    width: '100%',
+    height: '100%',
+    borderRadius: '10px',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
+  const goToPrevious = () => {
+    let newIndex;
+    console.log(slides.length - 1);
+    if (currentIndex === 0) {
+      newIndex = 2;
+    } else {
+      newIndex = currentIndex - 1;
+    }
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = useCallback(() => {
+    console.log(currentIndex);
+    let newIndex;
+    if (currentIndex === 2) {
+      newIndex = 0;
+    } else {
+      newIndex = currentIndex + 1;
+    }
+    setCurrentIndex(newIndex);
+  }, [currentIndex, slides]); //on définit les dépendances
+
+  useEffect(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(() => {
+      goToNext();
+    }, 4000);
+
+    return () => clearTimeout(timerRef.current);
+  }, [goToNext]);
+
+  return (
+    <div style={SliderStyle}>
+      <div style={leftArrowStyles} onClick={goToPrevious}>
+        ❰
+      </div>
+      <div style={rightArrowStyles} onClick={goToNext}>
+        ❱
+      </div>
+      <div style={SlideStyle}></div>
+    </div>
+  );
+};
+
+export default ImageSlider;
