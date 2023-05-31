df1945f65140faae2f8307739834f2e7be89bd10
Pietro <pietro.izzo@student-cs.fr>
2023-05-31 16:54:35 +0200

    Added movie adding script

--- frontend/src/pages/MovieList/MovieList.jsx
+++ frontend/src/pages/MovieList/MovieList.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import AddMovieForm from '../../components/AddMovieForm/AddMovieForm';
 import MoviesTable from '../../components/MoviesTable/MoviesTable';
+import AddMovieButton from '../../components/AddMovieButton/AddMovieButton';
 
 function Movies() {
   console.log('Avant déclaration de movies');
@@ -38,7 +39,13 @@ function Movies() {
   return (
     <div className="Movies-container">
       <h1>This page displays the movies</h1>
+
+      <p>Click this button to add the most popular movies to the database</p>
+      <AddMovieButton onSuccessfulMovieCreation={fetchMovies}></AddMovieButton>
+
+      <p>Or add movie based on information</p>
       <AddMovieForm onSuccessfulMovieCreation={fetchMovies} />
+
       <MoviesTable movies={movies} onSuccessfulMovieDeletion={fetchMovies} />
       {moviesLoadingError !== null && (
         <div className="movies-loading-error">{moviesLoadingError}</div>
